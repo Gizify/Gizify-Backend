715695d02d3bc9c22ff4e66b5aca36c844d1c6f0
farid <valkanid12@gmail.com>
2025-04-17 22:23:01 +0700

    perbaikan perhitungan

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -17,6 +17,8 @@ const scanProduct = async (req, res) => {
         brand: cachedProduct.brand,
         nutrition: cachedProduct.nutrition_info,
         ingredients: cachedProduct.ingredients_list,
+        packageSize: cachedProduct.package_size,
+        servingSize: cachedProduct.serving_size,
         cache: true,
       });
     }
@@ -26,29 +28,57 @@ const scanProduct = async (req, res) => {
     if (response.data.status === 1) {
       const product = response.data.product;
 
+      const packageSize = product.product_quantity || (product.quantity && parseFloat(product.quantity)) || null;
+
+      const servingSize = product.serving_quantity ? parseFloat(product.serving_quantity) : null;
+
+      const calculateNutrition = (nutrientPer100, nutrientValue, nutrientUnit) => {
+        if (nutrientValue && packageSize) {
+          if (nutrientUnit === "serving" && servingSize) {
+            return (nutrientValue / servingSize) * packageSize;
+          }
+
+          if (nutrientPer100) {
+            return (nutrientValue / 100) * packageSize;
+          }
+
+          return nutrientValue;
+        }
+
+        return nutrientValue;
+      };
+
+      const nutriments = product.nutriments || {};
+
+      const nutrition_info = {
+        calories: calculateNutrition(nutriments["energy-kcal_100g"], nutriments["energy-kcal"], nutriments["energy-kcal_unit"]),
+        carbs: calculateNutrition(nutriments.carbohydrates_100g, nutriments.carbohydrates, nutriments.carbohydrates_unit),
+        protein: calculateNutrition(nutriments.proteins_100g, nutriments.proteins, nutriments.proteins_unit),
+        fat: calculateNutrition(nutriments.fat_100g, nutriments.fat, nutriments.fat_unit),
+        sugar: calculateNutrition(nutriments.sugars_100g, nutriments.sugars, nutriments.sugars_unit),
+        sodium: calculateNutrition(nutriments.sodium_100g, nutriments.sodium, nutriments.sodium_unit),
+        fiber: calculateNutrition(nutriments.fiber_100g, nutriments.fiber, nutriments.fiber_unit),
+      };
+
       const newFoodCache = new FoodBarcode({
         barcode: product.code,
         product_name: product.product_name,
         brand: product.brands,
-        nutrition_info: {
-          calories: product.nutriments?.energy_kcal,
-          carbs: product.nutriments?.carbohydrates,
-          protein: product.nutriments?.proteins,
-          fat: product.nutriments?.fat,
-          sugar: product.nutriments?.sugars,
-          sodium: product.nutriments?.sodium,
-          fiber: product.nutriments?.fiber,
-        },
+        nutrition_info: nutrition_info,
         ingredients_list: product.ingredients_text || [],
+        package_size: packageSize,
+        serving_size: servingSize,
       });
 
       await newFoodCache.save();
-      I;
+
       res.json({
         name: product.product_name,
         brand: product.brands,
-        nutrition: newFoodCache.nutrition_info,
+        nutrition: nutrition_info,
         ingredients: newFoodCache.ingredients_list,
+        packageSize: packageSize,
+        servingSize: servingSize,
         cache: false,
       });
     } else {
