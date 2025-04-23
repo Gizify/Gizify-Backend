a0ec21e56f5097de9fb1aabe7d8ad760a760959a
farid <valkanid12@gmail.com>
2025-04-24 00:55:36 +0700

    perbaikan waktu stats

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,11 @@
 const User = require("../models/User");
 const FoodBarcode = require("../models/FoodBarcode");
+const { DateTime } = require("luxon");
 
 const addConsumptionFromBarcode = async (req, res) => {
   try {
     const { userId } = req;
-    const { barcode, portion_size = 1 } = req.body;
+    const { barcode, portion_size = 1, userTimeZone = "Asia/Jakarta" } = req.body;
 
     const foodItem = await FoodBarcode.findOne({ barcode });
     if (!foodItem) {
@@ -14,8 +15,9 @@ const addConsumptionFromBarcode = async (req, res) => {
     const user = await User.findById(userId);
     if (!user) return res.status(404).json({ message: "User tidak ditemukan" });
 
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
+    const userToday = DateTime.now().setZone(userTimeZone).startOf("day");
+    const todayStart = userToday.toJSDate();
+    const todayEnd = userToday.endOf("day").toJSDate();
 
     const adjustedNutrition = {
       calories: foodItem.nutrition_info.calories * portion_size,
@@ -27,11 +29,14 @@ const addConsumptionFromBarcode = async (req, res) => {
       sodium: foodItem.nutrition_info.sodium * portion_size,
     };
 
-    const statsIndex = user.nutrition_stats.findIndex((entry) => new Date(entry.date).toDateString() === today.toDateString());
+    const statsIndex = user.nutrition_stats.findIndex((entry) => {
+      const entryDate = DateTime.fromJSDate(new Date(entry.date)).setZone(userTimeZone).startOf("day");
+      return entryDate.equals(userToday);
+    });
 
     if (statsIndex === -1) {
       user.nutrition_stats.push({
-        date: today,
+        date: todayStart,
         total_calories: adjustedNutrition.calories,
         total_protein: adjustedNutrition.protein,
         total_carbs: adjustedNutrition.carbs,
@@ -50,7 +55,10 @@ const addConsumptionFromBarcode = async (req, res) => {
       user.nutrition_stats[statsIndex].total_sodium += adjustedNutrition.sodium;
     }
 
-    let todayMealLog = user.meal_logs.find((log) => new Date(log.date).toDateString() === today.toDateString());
+    let todayMealLog = user.meal_logs.find((log) => {
+      const logDate = DateTime.fromJSDate(new Date(log.date)).setZone(userTimeZone).startOf("day");
+      return logDate.equals(userToday);
+    });
 
     const mealEntry = {
       source: "barcode",
@@ -58,12 +66,12 @@ const addConsumptionFromBarcode = async (req, res) => {
       name: foodItem.product_name,
       portion_size: portion_size,
       nutrition_info: adjustedNutrition,
-      consumed_at: new Date(),
+      consumed_at: DateTime.now().setZone(userTimeZone).toJSDate(),
     };
 
     if (!todayMealLog) {
       user.meal_logs.push({
-        date: today,
+        date: todayStart,
         meals: [mealEntry],
       });
     } else {
@@ -72,13 +80,30 @@ const addConsumptionFromBarcode = async (req, res) => {
 
     await user.save();
 
-    const finalStats = user.nutrition_stats.find((entry) => new Date(entry.date).toDateString() === today.toDateString());
-    const finalMeals = user.meal_logs.find((entry) => new Date(entry.date).toDateString() === today.toDateString());
+    const finalStats = user.nutrition_stats.find((entry) => {
+      const entryDate = DateTime.fromJSDate(new Date(entry.date)).setZone(userTimeZone).startOf("day");
+      return entryDate.equals(userToday);
+    });
+
+    const finalMeals = user.meal_logs.find((entry) => {
+      const entryDate = DateTime.fromJSDate(new Date(entry.date)).setZone(userTimeZone).startOf("day");
+      return entryDate.equals(userToday);
+    });
 
     res.json({
       message: "Konsumsi berhasil ditambahkan",
-      today_stats: finalStats,
-      today_meals: finalMeals?.meals || [],
+      today_stats: finalStats
+        ? {
+            ...finalStats._doc,
+            date: DateTime.fromJSDate(finalStats.date).setZone(userTimeZone).toISO(),
+          }
+        : null,
+      today_meals:
+        finalMeals?.meals.map((meal) => ({
+          ...meal._doc,
+          consumed_at: DateTime.fromJSDate(meal.consumed_at).setZone(userTimeZone).toISO(),
+        })) || [],
+      timezone: userTimeZone,
     });
   } catch (err) {
     console.error("Error menambahkan konsumsi:", err);
