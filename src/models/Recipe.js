7594d684b465a5c64ba4b28d3abcfeb7c86678ee
farid <valkanid12@gmail.com>
2025-04-24 18:45:57 +0700

    perbikan model resep

--- src/models/Recipe.js
+++ src/models/Recipe.js
@@ -12,13 +12,11 @@ const IngredientSchema = new mongoose.Schema(
 
 const RecipeSchema = new mongoose.Schema({
   title: String,
-  author_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   ingredients: [IngredientSchema],
   steps: [String],
   nutrition_info: NutritionInfoSchema,
   created_at: { type: Date, default: Date.now },
   tags: [String],
-  visibility: { type: String, enum: ["public", "private"], default: "public" },
 });
 
 module.exports = mongoose.model("Recipe", RecipeSchema);
