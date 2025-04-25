dcf4d88dfa092ee8e9d66710700284a51e2e02b0
farid <valkanid12@gmail.com>
2025-04-25 14:56:47 +0700

    deploy vercel

--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const recipeController = require("../controllers/recipeController");
 
-
+// recipe route
 router.post("/generate", recipeController.generateRecipe);
 router.get("/", recipeController.getAllRecipes);
 
