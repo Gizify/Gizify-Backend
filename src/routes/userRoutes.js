0f847cff3feae9dbce5a6781df2081cdd1505e98
farid <valkanid12@gmail.com>
2025-04-27 22:26:58 +0700

    penghapusan route add konsumsi barcode

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,6 @@ const userController = require("../controllers/userController");
 
 const router = express.Router();
 
-router.post("/add-consumption", userController.addConsumptionFromBarcode);
 router.post("/consumption", userController.addConsumption);
 router.post("/generate", userController.generateRecipe);
 
