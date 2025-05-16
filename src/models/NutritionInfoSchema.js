a4749a0a4a45e5b1c1624562a55969a132cdb964
farid <valkanid12@gmail.com>
2025-05-16 22:18:17 +0700

    perbaikan model user

--- src/models/NutritionInfoSchema.js
+++ src/models/NutritionInfoSchema.js
@@ -3,12 +3,22 @@ const mongoose = require("mongoose");
 const NutritionInfoSchema = new mongoose.Schema(
   {
     calories: Number,
-    carbs: Number,
     protein: Number,
+    carbs: Number,
     fat: Number,
+    fiber: Number,
     sugar: Number,
     sodium: Number,
-    fiber: Number,
+    folic_acid: Number,
+    kalsium: Number,
+    vitamin_d: Number,
+    vitamin_b16: Number,
+    vitamin_b12: Number,
+    vitamin_c: Number,
+    zinc: Number,
+    iodium: Number,
+    water: Number,
+    iron: Number,
   },
   { _id: false }
 );
--- src/models/User.js
+++ src/models/User.js
@@ -1,52 +1,6 @@
 const mongoose = require("mongoose");
 const MealLogSchema = require("./MealLog");
-
-const NutritionStatsSchema = new mongoose.Schema(
-  {
-    date: { type: Date, required: true },
-    total_calories: Number,
-    total_protein: Number,
-    total_carbs: Number,
-    total_fat: Number,
-    total_fiber: Number,
-    total_sugar: Number,
-    total_sodium: Number,
-    total_folic_acid: Number,
-    total_kalsium: Number,
-    total_vitamin_d: Number,
-    total_vitamin_b16: Number,
-    total_vitamin_b12: Number,
-    total_vitamin_c: Number,
-    total_zinc: Number,
-    total_iodium: Number,
-    total_water: Number,
-    total_iron: Number,
-  },
-  { _id: false }
-);
-
-const DailyTargetSchema = new mongoose.Schema(
-  {
-    calories: Number,
-    protein: Number,
-    carbs: Number,
-    fat: Number,
-    fiber: Number,
-    sugar: Number,
-    sodium: Number,
-    folic_acid: Number,
-    kalsium: Number,
-    vitamin_d: Number,
-    vitamin_b16: Number,
-    vitamin_b12: Number,
-    vitamin_c: Number,
-    zinc: Number,
-    iodium: Number,
-    water: Number,
-    iron: Number,
-  },
-  { _id: false }
-);
+const NutritionInfoSchema = require("./NutritionInfoSchema");
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -57,9 +11,9 @@ const UserSchema = new mongoose.Schema({
   height: Number,
   weight: Number,
   activity_level: { type: String, enum: ["Ringan", "Sedang", "Berat"] },
-  daily_nutrition_target: DailyTargetSchema,
+  daily_nutrition_target: NutritionInfoSchema,
   meal_logs: [MealLogSchema],
-  nutrition_stats: [NutritionStatsSchema],
+  nutrition_stats: [NutritionInfoSchema],
   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Recipe" }],
 });
 
